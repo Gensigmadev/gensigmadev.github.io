737f7d54a184e3e7be1a3597c7483ae57f8978e5
GenzoFormosa <genquintana@gmail.com>
2024-06-04 07:53:44 -0300

    Se agrego API
    Se agregó api de video

--- scripts.js
+++ scripts.js
@@ -8,3 +8,48 @@ function showVideo() {
         allowfullscreen></iframe>
     `;
 }
+
+
+// Obtener el botón que abre el modal
+var btn = document.getElementById('loginBtn');
+
+// Obtener el modal
+var modal = document.getElementById('loginModal');
+
+// Obtener el botón de cierre del modal
+var span = document.getElementsByClassName('close')[0];
+
+// Obtener el botón de inicio de sesión
+var loginSubmit = document.getElementById('loginSubmit');
+
+// Cuando el usuario hace clic en el botón, abre el modal
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+// Cuando el usuario hace clic en <span> (x), cierra el modal
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+// Cuando el usuario hace clic fuera del modal, cierra el modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+// Verificación de entrada en el formulario de inicio de sesión
+loginSubmit.onclick = function() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Verificar si los campos están vacíos
+    if (!username.trim() || !password.trim()) {
+        // Mostrar mensaje de error
+        alert('Por favor, completa ambos campos');
+    } else {
+        // Si ambos campos están completos, enviar el formulario
+        document.getElementById('loginForm').submit();
+    }
+}
